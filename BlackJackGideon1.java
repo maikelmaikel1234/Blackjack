321d2954627c632ae82ae8de4ed0fa9e4556a323
Gideon Goderski <g.goderski@workingspirit.nl>
2023-01-18 16:10:57 +0100

    Tweede upload

---
+++ BlackJackGideon1.java
@@ -0,0 +1,28 @@
+package Blackjack;
+
+class BlackjackBlackJackGideon1 {
+	public static void main(String[] args) {
+	
+	System.out.println("Welkom bij Black Jack '21'");
+	
+	Game blackJack = new Game();
+		
+	}
+	
+}
+
+class Game {
+	
+	private int wins, losses, pass;
+	public Game() {
+		
+		wins = 0; 
+		losses = 0; 
+		pass = 0;
+	}
+}
+
+// class enum Suit {
+	
+	
+//}
