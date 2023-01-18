850d73ac36fde0951c579976f4749f45c4b77a28
Your Name <you@example.com>
2023-01-18 16:08:07 +0100

    test

---
+++ Alex/Spel.java
@@ -0,0 +1,12 @@
+package Blackjack.Alex;
+
+public class Spel {
+
+	public static void main(String[] args) {
+		System.out.println("Alex Popov's Blackjack");
+
+		int h2 = 1; 
+		System.out.println(h2);
+	}
+
+}
