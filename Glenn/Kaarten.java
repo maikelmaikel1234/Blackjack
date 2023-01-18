4edbfc5531cb22610b995ff669d10d7e4f412f66
G. Bakker <g.bakker@workingspirit.nl>
2023-01-18 16:09:37 +0100

    start kaarten

---
+++ Glenn/Kaarten.java
@@ -0,0 +1,17 @@
+package Blackjack.Glenn;
+
+public class Kaarten {
+	
+	static String[] deck = new String[52];
+	
+	static void schudKaarten() {
+		System.out.println("\nKaarten worden geschud.");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welkom bij Blackjack!");
+		schudKaarten();
+
+	}
+
+}
