21a21d139d8f203dd7cd553dee4465d17c746764
Maikel Schepers <m.schepers@workingspirit.nl>
2023-01-25 14:20:34 +0100

    push nr master

--- BlackjackMaikel/BlackjackMaikel.java
+++ BlackjackMaikel/BlackjackMaikel.java
@@ -1,9 +1,10 @@
-package BlackjackMaikel;
+package Blackjack;
 import java.util.*;
 
 public class BlackjackMaikel {
 
 	public static void main(String[] args) {
+		Kaarten[] cards = new Kaarten[52];
 		
 		Kaarten k = new Kaarten();
 		Scanner sc = new Scanner(System.in);
@@ -11,6 +12,7 @@ public class BlackjackMaikel {
 		System.out.println("Welkom bij blackjack.....");
 		System.out.println("Wilt u spelen (ja) (nee)");
 		String Antwoord = sc.nextLine();
+		if (Antwoord.equals("ja")) {
 		System.out.println("type ''schudden'' om kaarten te schudden");
 		String schud = sc.nextLine();
 		
@@ -25,11 +27,11 @@ public class BlackjackMaikel {
 		System.out.println("");
 		System.out.println("wilt u nog een extra kaart pakken. extra kaart(k), stoppen(q)");
 		String door = sc.next();
-
-		if (door.equals("k")) {
-			k.extraKaart();
-		}
+			while (door.equals("k")) {
+				k.extraKaart();
+			}
 		
+		}
 		
 			
 
--- BlackjackMaikel/Kaarten.java
+++ BlackjackMaikel/Kaarten.java
@@ -1,21 +1,18 @@
-package BlackjackMaikel;
+package Blackjack;
 import java.util.*;
 
 
 public class Kaarten {
 	Random rand = new Random();
 	
-
+	
 	String[] Kaart = {
 			"HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HB", "HQ", "HK",
 			"RA", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "RB", "RQ", "RK",
 			"SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SB", "SQ", "SK",
 			"KA", "K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "KB", "KQ", "KK"};
 	
-	int[] nr = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
-			,11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
-			,11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
-			,11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+
 	ArrayList<String> deck = new ArrayList<String>(Arrays.asList(Kaart));
 	
 	
@@ -43,6 +40,9 @@ public class Kaarten {
 	}
 	void extraKaart() {
 		System.out.println("U heeft als extra kaart "+deck.get(4+1)+" getrokken");
+		System.out.println("U heeft als extra kaart "+deck.get(4+1)+" getrokken");
+		System.out.println("U heeft als extra kaart "+deck.get(4+1)+" getrokken");
+		System.exit(0);
 		
 	}
 	
